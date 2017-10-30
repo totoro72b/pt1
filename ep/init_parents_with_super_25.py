0e598f662d721e0e644818dfe1029f1562c19e18
totoro <totoro72b@gmail.com>
2017-10-29 23:31:35 -0400

    use super() shorthand python3

--- ep/init_parents_with_super_25.py
+++ ep/init_parents_with_super_25.py
@@ -29,17 +29,18 @@ class TimesTwoPlusFiveBad(TimesTwoBad, PlusFiveBad):
 
 class PlusFive(BaseClass):
     def __init__(self, num):
-        super(PlusFive, self).__init__(num)
+        super().__init__(num)
         self.num += 5
 
 
 class TimesTwo(BaseClass):
     def __init__(self, num):
-        super(TimesTwo, self).__init__(num)
+        super().__init__(num)
         self.num *= 2
 
 
 class TimesTwoPlusFive(TimesTwo, PlusFive):
     """Illustrate that using super just works"""
     def __init__(self, num):
-        super(__class__, self).__init__(num)
+        # super(__class__, self).__init__(num)
+        super().__init__(num)
