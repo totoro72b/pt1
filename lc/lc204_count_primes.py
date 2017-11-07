3d125b9eb3b1dbed2584ddaa2f0721f12452ade0
totoro <totoro72b@gmail.com>
2017-11-07 00:07:59 -0500

    add better version of prime counter

--- lc/lc204_count_primes.py
+++ lc/lc204_count_primes.py
@@ -1,3 +1,5 @@
+import math
+
 class Solution(object):
     def countPrimes_slow(self, n):
         """Return the number of primes p where p < n and n >= 0"""
@@ -15,5 +17,34 @@ class Solution(object):
             if is_prime:
                 primes.append(k)
         return len(primes)
+
+    def countPrimes(self, n):
+        """Return the number of primes p where p < n and n >= 0 (fast version)"""
+        if n <= 2:
+            return 0
+        # now at least [0, 1, 2, ...]
+        primes = [True] * n
+        primes[0], primes[1] = False, False
+        for k in range(2, n):
+            if primes[k]:
+                # knock out multiples of k
+                primes[k*2:n:k] = [False] * len(primes[k*2:n:k])
+        return primes.count(True)
+
+    def countPrimesImproved(self, n):
+        """Return the number of primes p where p < n and n >= 0 (fast version)"""
+        if n <= 2:
+            return 0
+        # now at least [0, 1, 2, ...]
+        primes = [True] * n
+        primes[0], primes[1] = False, False
+        for k in range(2, math.ceil(n**0.5)):  # NOTE: since we start at k*k, it's meaningless empty set when k > sqrt(n)
+            if primes[k]:
+                # knock out multiples of k
+                # NOTE: Improved: start from k*k because k*2 shoulda been knocked out by prime 2
+                primes[k*k:n:k] = [False] * len(primes[k*k:n:k])
+        return primes.count(True)
+
+
 
 
--- lc/tests/test_lc204_count_primes.py
+++ lc/tests/test_lc204_count_primes.py
@@ -6,19 +6,32 @@ from lc204_count_primes import Solution
 class TestCountPrimes(unittest.TestCase):
     def setUp(self):
         self.soln = Solution()
+        self.test_primes = [
+        (0, 0),
+        (1, 0),
+        (2, 0),
+        (3, 1),
+        (4, 2),
+        (5, 2),
+        (6, 3),
+        (7, 3),
+        (8, 4),
+        (9, 4),
+        (10, 4),
+        (11, 4),
+        (12, 5)]
 
     def test_count_primes_slow(self):
-        """Test some simple cases"""
-        self.assertEqual(self.soln.countPrimes_slow(0), 0)
-        self.assertEqual(self.soln.countPrimes_slow(1), 0)
-        self.assertEqual(self.soln.countPrimes_slow(2), 0)
-        self.assertEqual(self.soln.countPrimes_slow(3), 1)
-        self.assertEqual(self.soln.countPrimes_slow(4), 2)
-        self.assertEqual(self.soln.countPrimes_slow(5), 2)
-        self.assertEqual(self.soln.countPrimes_slow(6), 3)
-        self.assertEqual(self.soln.countPrimes_slow(7), 3)
-        self.assertEqual(self.soln.countPrimes_slow(8), 4)
-        self.assertEqual(self.soln.countPrimes_slow(9), 4)
-        self.assertEqual(self.soln.countPrimes_slow(10), 4)
-        self.assertEqual(self.soln.countPrimes_slow(11), 4)
-        self.assertEqual(self.soln.countPrimes_slow(12), 5)
+        """Slow version: Test some simple cases"""
+        for n, num_primes in self.test_primes:
+            self.assertEqual(self.soln.countPrimes_slow(n), num_primes)
+
+    def test_count_primes(self):
+        """better version: Test some simple cases"""
+        for n, num_primes in self.test_primes:
+            self.assertEqual(self.soln.countPrimes(n), num_primes)
+
+    def test_count_primes_improved(self):
+        """Improved: Test some simple cases"""
+        for n, num_primes in self.test_primes:
+            self.assertEqual(self.soln.countPrimesImproved(n), num_primes)
