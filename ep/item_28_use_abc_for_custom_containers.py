fddb1687d56fe5763a45428b18440e1a227e76ab
totoro <totoro72b@gmail.com>
2017-11-09 23:25:42 -0500

    still need to debug

---
+++ ep/item_28_use_abc_for_custom_containers.py
@@ -0,0 +1,62 @@
+# when need to define customized container types, inherit from abc to make sure you don't miss shit
+
+# by subclassing from list, you get all of list's standard functionalities. no need to use abc
+class FrequencyList(list):
+    """A list that also returns frequency"""
+    def __init__(self, stuff):
+        super().__init__(stuff)
+
+    def get_frequency(self):
+        d = {}
+        total = 0
+        for item in self:
+            d.setdefault(item, 0)
+            d[item] += 1
+            total += 1
+
+        for k, v in d.items():
+            d[k] = v/total
+
+        return d
+
+# but when building something that acts like a list but doesn't inherit from list,
+# use abc to help you reinforce the interface
+class BinaryNode(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class IndexableNode(BinaryNode):
+    count = 0
+
+    def _traversal(self, node, n):
+        if not node:
+            return False, node
+        print('node=', node.value)
+
+        if node.left:
+            print('go left')
+            found, x = self._traversal(node.left, n)
+            if found:
+                return found, x
+        self.count += 1
+        if self.count == n:
+            print('inc count, bingo now=', self.count)
+            return (True, node)
+
+        if node.right:
+            print('go right')
+            found, x = self._traversal(node.left, n)
+            if found:
+                return found, x
+        return False, node
+
+    def __getitem__(self, index):
+        """return the ith node of the binary tree (zero indexed)"""
+        self.count = 0
+        found, node = self._traversal(self, index+1)
+        if found:
+            return node
+        raise IndexError
---
+++ ep/tests/test_item_28_use_abc_for_custom_containers.py
@@ -0,0 +1,29 @@
+import unittest
+
+from item_28_use_abc_for_custom_containers import (FrequencyList,
+                                                   BinaryNode,
+                                                   IndexableNode)
+
+class TestABC(unittest.TestCase):
+    def test_get_frequency(self):
+        """Test FrequencyList"""
+        stuff = ('a', 'b', 'a', 'c')
+        fl = FrequencyList(stuff)
+        f = fl.get_frequency()
+        self.assertEqual(f['a'], 0.5)
+        self.assertEqual(f['b'], 0.25)
+        self.assertEqual(f['c'], 0.25)
+        self.assertEqual(type(fl), FrequencyList)
+        self.assertEqual(len(fl), len(stuff))  # can use len method
+        self.assertEqual(repr(fl), "['a', 'b', 'a', 'c']")  # can use repr too
+
+    def test_indexable(self):
+        """Test indexable binary nodes stuff"""
+        root = IndexableNode(3)
+        root.left = IndexableNode(1, None, IndexableNode(2))
+        root.right = IndexableNode(4)
+
+        # self.assertEqual(root[0].value, 1)
+        self.assertEqual(root[1].value, 2)
+        # self.assertEqual(root[2].value, 3)
+        # self.assertEqual(root[3].value, 4)
