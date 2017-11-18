4e9becd185fdf253185cac02dd80891ab5a04e37
totoro <totoro72b@gmail.com>
2017-11-18 00:05:53 -0500

    item 34 passes tests

--- ep/item_34_register_class_existence_with_metaclasses.py
+++ ep/item_34_register_class_existence_with_metaclasses.py
@@ -1,75 +1,39 @@
 import json
 
 
-registry = {}  # class name -> class
+registry = {}
 
 def register(cls):
-    registry[cls.__class__.__name__] = cls
-
-def register_class(cls):
-    registry[cls.__class__.__name__] = cls
+    registry[cls.__name__] = cls
 
 
-def deserialize(serialized):
-    params = json.loads(serialized)
-    print(registry)
-    cls = registry[params['class_name']]
-    return cls(*params['args'])
-
-
-class RegisterMeta(type):
-    def __new__(meta, name, bases, class_dict):
-        """Register class under class name"""
+class Meta(type):
+    def __new__(meta, name, bases, class_dict):  # m nbc
         cls = type.__new__(meta, name, bases, class_dict)
         register(cls)
         return cls
 
 
-# use metaclasses to register classes, so you can deserialize a class super easily!
-class Serializable(object, metaclass=RegisterMeta):
+class Serializable(object, metaclass=Meta):
+    """Provide method to serialize the args of a class"""
     def __init__(self, *args):
         self.args = args
 
     def serialize(self):
-        """Serialize the args for this object"""
         return json.dumps({'args': self.args,
-                           'class_name': self.__class__.__name__})
+                           'class': self.__class__.__name__
+                          })
 
 
-class Point3D(Serializable):
-    """Describe a 3D point"""
+def deserialize(serialized):
+    params = json.loads(serialized)
+    cls = registry[params['class']]
+    return cls(*params['args'])
+
+
+class Point3Db(Serializable):
     def __init__(self, x, y, z):
         super().__init__(x, y, z)
-        self.x, self.y, self.z = x, y, z
-
-
-############ code from book
-class BetterSerializable(object):
-    def __init__(self, *args):
-        self.args = args
-
-    def serialize(self):
-        return json.dumps({
-            'class': self.__class__.__name__,
-            'args': self.args,
-        })
-    def __repr__(self):
-        pass
-
-
-class Meta(type):
-    def __new__(meta, name, bases, class_dict):
-        cls = type.__new__(meta, name, bases, class_dict)
-        register_class(cls)
-        return cls
-
-
-class RegisteredSerializable(BetterSerializable,
-                             metaclass=Meta):
-    pass
-
-
-class Vector3D(RegisteredSerializable):
-    def __init__(self, x, y, z):
-        super().__init__(x, y, z)
-        self.x, self.y, self.z = x, y, z
+        self.x = x
+        self.y = y
+        self.z = z
--- ep/tests/test_item_34_register_class_existence_with_metaclasses.py
+++ ep/tests/test_item_34_register_class_existence_with_metaclasses.py
@@ -1,26 +1,16 @@
 import unittest
 
-from item_34_register_class_existence_with_metaclasses import Point3D, deserialize, Vector3D
+from item_34_register_class_existence_with_metaclasses import Point3Db, deserialize
 
 
 class TestMetaRegistration(unittest.TestCase):
     """Test the usage of meta classes to register classes for easy deserialization"""
 
-    # def test_serialization(self):
-        # """test serialize an object"""
-        # pt = Point3D(1,2,3)
-        # serialized = pt.serialize()
-        # obj = deserialize(serialized)
-        # self.assertEqual(pt.x, obj.x)
-        # self.assertEqual(pt.y, obj.y)
-        # self.assertEqual(pt.z, obj.z)
-
-    def test_book_example(self):
-        """Why works in the book but not in my code?"""
-        v3 = Vector3D(10, -7, 3)
-        data = v3.serialize()
-        obj = deserialize(data)
+    def test_auto_register_with_meta(self):
+        """Test auto register with metaclass for deserialization"""
+        p3 = Point3Db(10, -7, 3)
+        serialized = p3.serialize()
+        obj = deserialize(serialized)
         self.assertEqual(obj.x, 10)
         self.assertEqual(obj.y, -7)
         self.assertEqual(obj.z, 3)
-
