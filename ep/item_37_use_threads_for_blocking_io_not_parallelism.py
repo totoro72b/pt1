38110e4b34308e2e65c8b81bc6620335365f28df
totoro <totoro72b@gmail.com>
2017-12-03 21:49:29 -0500

    done with item 37

--- ep/item_37_use_threads_for_blocking_io_not_parallelism.py
+++ ep/item_37_use_threads_for_blocking_io_not_parallelism.py
@@ -1,5 +1,6 @@
 import time
 import threading
+import select
 
 # use threads for blocking io, not for parallelism
 # for example do something and try out the time
@@ -65,6 +66,38 @@ print('threads used %.3f' % (end - start))
 # end = time.time()
 # print('threads approach 2 used %.3f' % (end - start))
 
+# ***** however, you can use threads for doing sys calls for blocking IO. it DOES run in parallel in that case
 
-#### however, you can use threads for doing sys calls for blocking IO. it DOES run in parallel in that case
+def slow_sys_call():
+    select.select([], [], [], 0.1)  # a sys call that takes 0.1s long
 
+
+def without_threads():
+    for i in range(5):
+        slow_sys_call()
+
+
+start = time.time()
+without_threads()
+end = time.time()
+print('sys call without threads, it took %.3fs' % (end - start))
+
+
+def compute_helicopter_location():
+    """something to do meanwhile"""
+    pass
+
+threads = []
+start = time.time()
+for i in range(5):
+    t = threading.Thread(target=slow_sys_call)
+    t.start()
+    threads.append(t)
+
+compute_helicopter_location()
+
+for t in threads:
+    t.join()
+
+end = time.time()
+print('sys call using threads, it took %.3fs' % (end - start))
