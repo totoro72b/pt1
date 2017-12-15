24eb358c1209adb0083aa208c421334bec202817
totoro <totoro72b@gmail.com>
2017-12-15 00:59:00 -0500

    wip: added ClosableQueue but only 1 item ended up in done queue

--- ep/item_39_use_queue_to_coordinate_work_between_threads.py
+++ ep/item_39_use_queue_to_coordinate_work_between_threads.py
@@ -1,4 +1,5 @@
 from collections import deque
+from queue import Queue
 from time import sleep
 import threading
 from threading import Lock
@@ -81,3 +82,90 @@ for t in workers:
     t.join()
 
 print('total num items processed %d. total times polled %d.' % (workers[-1].num_done, workers[-1].num_polled))
+
+################
+# try Queue class check out if task_done called order matters? or threads matter?
+q = Queue()
+# consumer
+def consume():
+    print('consumer waiting')
+    q.get()
+    q.task_done()
+    print('consumer done')
+
+# producer
+t = threading.Thread(target=consume)
+t.start()
+
+t2 = threading.Thread(target=consume)
+t2.start()
+
+for i in range(2):
+    q.put(object())
+print('producer waiting...')
+q.join()
+print('producer done')
+
+
+# a class for closable queue
+class ClosableQueue(Queue):
+    SENTINEL = object()
+
+    # def __init__(self):
+        # self.queue = Queue()
+
+    # def put(self, item):
+        # self.queue.put(item)
+
+    def close(self):
+        self.put(self.SENTINEL)
+
+    def __iter__(self):
+        try:
+            item = self.get()
+            if item is self.SENTINEL:
+                return  # exit the thread???
+            yield item
+        finally:
+            self.task_done()
+
+
+# Stoppable Worker that utilizes the closable queue
+class StoppableWorker(threading.Thread):
+    """a worker that gets an item from in_queue and transforms it and puts on the out_queue"""
+    def __init__(self, in_queue, out_queue, func):
+        super().__init__()
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+        self.func = func
+
+    def run(self):
+        for item in self.in_queue:  # this run method will exit when SENTINEL is reached in ClosableQueue
+            item = self.func(item)
+            self.out_queue.put(item)
+
+
+# create the pipes
+closable_download_queue = ClosableQueue()
+closable_resize_queue = ClosableQueue()
+closable_upload_queue = ClosableQueue()
+closable_done_queue = ClosableQueue()
+
+# fill in the initial items
+for i in range(100):
+    closable_download_queue.put(object())
+
+closable_download_queue.close()
+
+# line up the pipes
+links = [(closable_download_queue, closable_resize_queue, download),
+         (closable_resize_queue, closable_upload_queue, resize),
+         (closable_upload_queue, closable_done_queue, upload)]
+
+for q_in, q_out, func in links:
+    t = StoppableWorker(q_in, q_out, func)
+    t.start()
+
+closable_upload_queue.join()
+print('upload done')
+print('num items in done queue %d' % len([x for x in closable_done_queue]))
