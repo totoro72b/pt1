1428d412ab11d7917f71fb898c429091979f799e
totoro <rey72b@gmail.com>
2019-01-26 21:29:11 -0500

    prettier reformat

--- closet/src/app/app.component.ts
+++ closet/src/app/app.component.ts
@@ -16,21 +16,24 @@ export class AppComponent implements OnInit {
       {
         category: 'top',
         description: 'fav blouse',
-        imgUrl: 'https://images.anthropologie.com/is/image/Anthropologie/4110084326168_089_b?$an-category$&qlt=80&fit=constrain',
+        imgUrl:
+          'https://images.anthropologie.com/is/image/Anthropologie/4110084326168_089_b?$an-category$&qlt=80&fit=constrain',
         id: 1
       },
       {
         category: 'bottom',
         description: 'skirt',
-        imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7z09tXLDZODUEJLM5q6neP0NXLBhjckrZz6ZIXOzjBsLCP9NVrw',
+        imgUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7z09tXLDZODUEJLM5q6neP0NXLBhjckrZz6ZIXOzjBsLCP9NVrw',
         id: 2
       },
       {
         category: 'accessory',
         description: 'pink hat',
-        imgUrl: 'https://www.acnestudios.com/on/demandware.static/-/Sites-acne-product-catalog/default/dwdfbd534d/images/D4/D40001-/1500x/D40001-418_C.jpg',
+        imgUrl:
+          'https://www.acnestudios.com/on/demandware.static/-/Sites-acne-product-catalog/default/dwdfbd534d/images/D4/D40001-/1500x/D40001-418_C.jpg',
         id: 3
-      },
+      }
     ];
   }
 }
