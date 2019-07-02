4631e5673163fb87a5e633f8db70d3d8be1fb595
reyna <rey72b@gmail.com>
2019-07-02 09:04:03 -0400

    add stuff to store

---
+++ redux-store/src/store/store.ts
@@ -0,0 +1,12 @@
+class Store {
+  private subscribers: Function[];
+  private reducers: { [key: string]: Function };
+  private state: { [key: string]: any };
+
+  constructor(reducers = {}, initialState = {}) {
+    this.state = initialState;
+  }
+  get value() {
+    return this.state;
+  }
+}
