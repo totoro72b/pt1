7d70a2f47b5bc2d495055d536adbfb0d6be86d5f
reyna <rey72b@gmail.com>
2019-10-22 09:41:11 -0400

    add start end with concat

--- rxjs-test/src/lab2-count-down.ts
+++ rxjs-test/src/lab2-count-down.ts
@@ -1,5 +1,5 @@
 import { interval } from "rxjs";
-import { mapTo, scan, filter, tap, takeWhile } from "rxjs/operators";
+import { mapTo, scan, filter, tap, takeWhile, startWith } from "rxjs/operators";
 
 // lab 2 - count down timer
 const counter$ = interval(500);
@@ -15,15 +15,17 @@ const obs1$ = counter$.pipe(
 );
 obs1$.subscribe(val => console.log(val));
 
+const START_VALUE = 10;
+
 // lab 2 - count down timer fix
 const obs2$ = counter$.pipe(
   mapTo(-1),
-  scan((acc, val) => acc + val, 10),
-  tap({
-    next: (x: any) => console.log("tap with take while", x),
-    complete: () => console.log("complete-take while"),
-    error: (x: any) => console.log("error", x)
-  }), // tap shows us the observable stops emitting after encountering first -1
+  scan((acc, val) => acc + val, START_VALUE), // started with 10 and immediately -1, so first shown val =9
+  startWith("asdf"), // to fix starting on 9, we tag on 10
+  tap(x => console.log("obs2 stream", x)),
   takeWhile(x => x >= 0)
 );
-obs2$.subscribe(val => console.log(val));
+obs2$.subscribe({
+  next: val => console.log(val),
+  complete: () => console.log("obs2 complete!")
+});
