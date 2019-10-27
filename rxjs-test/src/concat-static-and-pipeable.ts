1642d4798030855033af8d4bfbc4b60605cbc0cd
totoro <rey72b@gmail.com>
2019-10-27 15:21:16 -0400

    asdf

--- rxjs-test/src/concat-static-and-pipeable.ts
+++ rxjs-test/src/concat-static-and-pipeable.ts
@@ -1,18 +1,18 @@
-import { of, concat, interval, empty } from "rxjs";
+import { of, concat, interval, empty } from 'rxjs';
 import {
   take,
   startWith,
   endWith,
   concat as _concat,
   delay
-} from "rxjs/operators";
+} from 'rxjs/operators';
 const numbers$ = of(1, 2, 3);
 
 // append values to stream
 numbers$
   .pipe(
-    startWith("a", "b", "c"),
-    endWith("d", "e", "f")
+    startWith('a', 'b', 'c'),
+    endWith('d', 'e', 'f')
   )
   .subscribe(console.log);
 
@@ -22,18 +22,19 @@ const source1$ = concat(interval$.pipe(take(3)), interval$);
 source1$.subscribe(console.log); // maintain order of execution
 
 //concat pipeable version
-const delayed$ = empty().pipe(delay(3000));
+// delay a random interval
+const delayed$ = empty().pipe(delay(Math.floor(Math.random() * 1000)));
 
 delayed$
   .pipe(
     // pipeable concat operator. maintain order of execution
     _concat(
-      delayed$.pipe(startWith("3...")),
-      delayed$.pipe(startWith("3...")),
-      delayed$.pipe(startWith("2...")),
-      delayed$.pipe(startWith("1...")),
-      delayed$.pipe(startWith("GO"))
+      delayed$.pipe(startWith('3...')),
+      delayed$.pipe(startWith('3...')),
+      delayed$.pipe(startWith('2...')),
+      delayed$.pipe(startWith('1...')),
+      delayed$.pipe(startWith('GO'))
     ),
-    startWith("Get ready...")
+    startWith('Get ready...')
   )
   .subscribe(console.log);
